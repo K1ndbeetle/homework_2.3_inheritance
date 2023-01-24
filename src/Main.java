b426151de0186b2b086157c2edd783643e8a6e41
K1ndbeetle <alx-s@bk.ru>
2023-01-24 23:55:09 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,86 @@
+import Transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car carLada = new Car(
+                "Lada",
+                "",
+                1.7,
+                "",
+                2015,
+                "Россия",
+                200,
+                "МКПП",
+                "лифтбэк",
+                "А808СА77",
+                5,
+                false,
+                null);
+
+        Car carAudi = new Car(
+                "Audi",
+                "A8 50 L TDI quattro",
+                3.0,
+                "черный",
+                0,
+                "Германия",
+                220,
+                "АКПП",
+                "седан",
+                "А888АА61",
+                5,
+                true,
+                new Car.Key(true, true));
+
+        Car carBmw = new Car(
+                null,
+                "Z8",
+                3.0,
+                "черный",
+                2021,
+                "Германия",
+                260,
+                "АКПП",
+                "кабриолет",
+                "Х888ХХ77",
+                5,
+                true,
+                new Car.Key(false, false));
+
+        Car carKia = new Car(
+                "null",
+                "Sportage 4-го поколения",
+                2.4,
+                "красный",
+                2018,
+                "",
+                -180,
+                "АКПП",
+                "кроссовер",
+                "Х404ХХ161",
+                5,
+                true,
+                new Car.Key(true, false));
+
+        Car carHyundai = new Car(
+                "Hyundai",
+                "Avante",
+                0,
+                "оранжевый",
+                2016,
+                "Южная Корея",
+                240,
+                "",
+                "седан",
+                "Х761ХХ761",
+                5,
+                true,
+                new Car.Key(false, true));
+
+        System.out.println(carLada + "\n" + carAudi + "\n" +  carBmw  + "\n" +  carKia  + "\n" +  carHyundai);
+
+        carLada.tireSeason(8);
+        System.out.println();
+        System.out.println(carLada);
     }
 }
---
+++ src/Transport/Transport.java
@@ -0,0 +1,76 @@
+package Transport;
+
+public class Transport {
+    private final String brand;
+    private final String model;
+    private final int productionYear;
+    private final String productionCountry;
+    private String color;
+    private int maxSpeed;
+
+    public Transport(String brand,
+                     String model,
+                     int productionYear,
+                     String productionCountry,
+                     String color,
+                     int maxSpeed) {
+        if (brand == "" || brand == null || brand == "null") {
+            this.brand = "default";
+        } else this.brand = brand;
+        if (model == "" || model == null || model == "null") {
+            this.model = "default";
+        } else this.model = model;
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        } else this.productionYear = productionYear;
+        if (productionCountry == "" || productionCountry == null || productionCountry == "null") {
+            this.productionCountry = "default";
+        } else this.productionCountry = productionCountry;
+        setColor(color);
+        setMaxSpeed(maxSpeed);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color == "" || color == null || color == "null") {
+            this.color = "белый";
+        } else this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        if (maxSpeed <= 0) {
+            this.maxSpeed = 200;
+        } else this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль : " +
+                "Марка " + brand + ", модель " + model +
+                ", цвет " + color + ", год выпуска " + productionYear + ", страна производства "
+                + productionCountry + ", максимальная скорость - " + maxSpeed;
+    }
+}
